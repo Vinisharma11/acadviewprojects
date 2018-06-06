4f402dc85bf3790e17d72b4f562937a2b1da00cd
Vinisharma11 <sharvi786@gmail.com>
2018-06-06 13:43:37 +0530

    1 commit

---
+++ assignment2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class First1{
+	public static void main(String []vini)
+	{
+		System.out.println("hello world");
+
+	}
+
+}
---
+++ assignment2a.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class First{
+	public static void main(String []vini)
+	{
+		System.out.println("my name is Vinita");
+
+	}
+
+}
